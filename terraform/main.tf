30ee50d9750914966da64cb92a0486db3db765cb
pradeem <73641627+pradeem@users.noreply.github.com>
2025-06-05 23:03:19 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  key_name      = var.key_name
+
+  tags = {
+    Name = "DevOpsWebServer"
+  }
+
+  provisioner "file" {
+    source      = "../app/index.html"
+    destination = "/tmp/index.html"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo mv /tmp/index.html /var/www/html/index.html",
+      "sudo service apache2 restart"
+    ]
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file(var.private_key_path)
+    host        = self.public_ip
+  }
+}
